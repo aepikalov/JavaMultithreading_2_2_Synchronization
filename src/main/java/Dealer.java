f2c269a68334437f9570676f0d657ee71ec94626
Artem Pikalov <aepikalov@gmail.com>
2021-10-22 01:05:26 +0400

    JavaMultithreading_2_2_Synchronization ver.2

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -5,41 +5,44 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Dealer {
-    private static final int RECIEVE_TIME = 3000;
+    private static final int RECEIVE_TIME = 3000;
     private static final int SELL_TIME = 1000;
-    private List<Car> cars = new ArrayList<>();
+    private static final int CARS = 10;
+    private final List<Car> cars = new ArrayList<>();
     private final Lock lock = new ReentrantLock();
     private final Condition condition = lock.newCondition();
 
     public void receiveCar() {
-        try {
-            lock.lock();
-            Thread.sleep(RECIEVE_TIME);
-            cars.add(new Car());
-            System.out.println(Thread.currentThread().getName() + " выпустил 1 авто.");
-            condition.signal();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
+        for (int i = 0; i < CARS; i++) {
+            try {
+                Thread.sleep(RECEIVE_TIME);
+                cars.add(new Car());
+                System.out.println(Thread.currentThread().getName() + " выпустил 1 авто.");
+                lock.lock();
+                condition.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
         }
     }
 
-    public Car sellCar() {
+    public void sellCar() {
         try {
             lock.lock();
             System.out.println(Thread.currentThread().getName() + " зашел в автосалон.");
-            while (cars.size() == 0) {
+            while (cars.isEmpty()) {
                 System.out.println("Машин нет!");
                 condition.await();
             }
             Thread.sleep(SELL_TIME);
             System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто.");
+            cars.remove(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             lock.unlock();
         }
-        return cars.remove(0);
     }
 }
